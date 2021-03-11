e258836ede3861d0a0c0d58921c2b3250809bb52
RONALDBUZAGLO <senseironald@gmail.com>
2021-03-11 13:39:57 -0400

    incluindo autenticação com JWT

--- index.js
+++ index.js
@@ -2,14 +2,60 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const PORT = 3050;
 
-app.use(cors());
+const jwtSecret = "asldhjubnakojrnglakjrbgkonj"
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+function auth(req,res,next){
+
+    const authToken = req.headers['authorization'];
+
+    if (authToken != undefined) {
+
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,jwtSecret,(err,data)=>{
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido"});
+            }else{
+                req.token = token;
+                req.loggedUser = {id:data.id,email:data.email};
+                next();
+            }
+        })
+
+    } else {
+        res.status(401);
+        res.json({err:"Token inválido"});
+    }
+
+
+}
+
+
 var DB = {
+    users:[
+        {
+            id:1,
+            name:"Ronald",
+            email:"ronald@ronald",
+            password:123
+        },
+        {
+            id:2,
+            name:"Buzaglo",
+            email:"buzaglo@buzaglo",
+            password:456
+        },
+    ],
     games:[
         {
             id:1,
@@ -110,7 +156,7 @@ var DB = {
     ]
 }
 
-app.get("/game",(req,res)=>{
+app.get("/game",auth,(req,res)=>{
     res.statusCode = 200;
     res.json(DB.games);
 });
@@ -196,6 +242,46 @@ app.put("/game/:id",(req,res)=>{
         res.sendStatus(404);
     }
 });
+
+app.post("/auth",(req,res)=>{
+
+    var {email,password} = req.body;
+
+    if(email != undefined){
+
+        var user = DB.users.find(user=>user.email == email);
+
+        if(user != undefined){
+
+            if(user.password == password){
+
+                jwt.sign({id:user.id,email:user.email},jwtSecret,{expiresIn:'48h'},(err,token)=>{
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token:token});
+                    }
+                })
+
+                
+            }else{
+                res.status(401);
+                res.json({erro:"credenciais inválidas"})
+            }
+
+        }else{
+            res.status(404);
+            res.json({err:"O email enviado é inválido"});
+        }
+
+    }else{
+        res.status(400);
+        res.json({err:"O email enviado é inválido"});
+    }
+
+})
 
 
 app.listen(PORT,()=>{
